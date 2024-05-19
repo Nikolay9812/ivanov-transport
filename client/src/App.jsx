5351b4f63aba7469951b3abbbb5776525ed0f6b6
Nikolay9812 <73443601+Nikolay9812@users.noreply.github.com>
2024-05-19 06:44:54 +0200

    install react js and taiwindcss and create the first template

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className="text-red-500 w-10 text-2xl">
+      <h2 className='text-4xl'>tralal  </h2>
+    </div>
+  )
+}
